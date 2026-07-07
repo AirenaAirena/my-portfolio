a277275f3d484cf4ab8a22d4158b31c1d003adb8
AirenaAirena <airenekara@gmail.com>
2026-07-07 13:23:21 +0200

    Delete src/components/Butterfly.jsx

--- src/components/Butterfly.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Lottie from 'lottie-react'
-import butterflyAnimation from '../assets/animations/butterfly.json'
-
-export default function Butterfly() {
-  return <Lottie animationData={butterflyAnimation} loop={true} />
-}
