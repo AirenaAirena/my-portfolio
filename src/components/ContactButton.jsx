d59874b77582672f60464119a1a26cd796996f13
AirenaAirena <airenekara@gmail.com>
2026-07-07 13:14:23 +0200

    Update ContactButton.jsx

--- src/components/ContactButton.jsx
+++ src/components/ContactButton.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import './ContactButton.css'
-import Lottie from 'lottie-react'
-import butterflyAnimation from '../assets/animations/butterfly.json'
+
+const Butterfly = lazy(() =>
+  Promise.all([
+    import('lottie-react'),
+    import('../assets/animations/butterfly.json'),
+  ]).then(([lottieModule, animationModule]) => {
+    const LottieComponent = lottieModule.default
+    return {
+      default: (props) => (
+        <LottieComponent
+          animationData={animationModule.default}
+          {...props}
+        />
+      ),
+    }
+  })
+)
 
 export default function ContactButton({ onClick, className = '' }) {
   return (
     <div className={`contact-button-wrapper ${className}`} onClick={onClick}>
       <div className="contact-label">Contact me</div>
 
-      <Lottie
-        animationData={butterflyAnimation}
-        loop={true}
-        className="butterfly-animation"
-      />
+      <Suspense fallback={<div className="butterfly-animation" />}>
+        <Butterfly loop={true} className="butterfly-animation" />
+      </Suspense>
     </div>
   )
 }
+
